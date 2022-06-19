ec385380580bd0e14db3d10b81a058156e33775d
Laucha0550 <ivangabrielf971@gmail.com>
2022-06-19 03:17:52 -0300

    Casi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./components/Login";
+import Login2 from "./components/Login2";
 import Cliente from "./components/Cliente";
 import Persona from "./components/Persona";
 import { BrowserRouter as Router, Routes, Route, Link, Outlet  } from "react-router-dom";
@@ -14,44 +15,14 @@ const SidebarLayout = () => (
 
 function App() {
   return (
-    //<Login />
     <Router>
-      {/* <h1>Todo oki</h1> */}
       <div className=" container inline-flex  ">
-        {/* <div className="">
-          <Link to="/cliente">
-            <button className=" my-5 p-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg ">
-              Cliente
-            </button>
-          </Link>
-        </div>
-        <div className="">
-          <Link to="/planta">
-            <button className=" my-5 p-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg ">
-              Nodos
-            </button>
-          </Link>
-        </div>
-        <div className=" ">
-          <Link to="/persona">
-            <button className=" my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg ">
-              Persona
-            </button>
-          </Link>
-        </div>
-        <div className=" ">
-          <Link to="/sensor">
-            <button className=" my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg ">
-              Nodos
-            </button>
-          </Link>
-        </div> */}
         <Routes>
-          <Route element={<SidebarLayout/>}>
+          <Route element={<SidebarLayout />}>
             <Route path="/cliente" element={<Cliente />}></Route>
             <Route path="/persona" element={<Persona />}></Route>
           </Route>
-          <Route path="/" exact element={<Login /> }></Route>
+          <Route path="/" exact element={<Login2 /> }></Route>
         </Routes>
       </div>
     </Router>
