5dde89e76732fe9aac9226da1d464a0dd1f6d533
lucasvasquez01 <lucasvasquez015@gmail.com>
2022-06-19 03:18:38 -0300

    tabla

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,8 +1,48 @@
 import React from "react";
+
 const Cliente = () => {
   return (
-    <div>
-      <div className="sm:grid-cols-2 w-screen h-full">
+    <div className=" grid grid-cols-1 sm:grid-cols-2 h-screen w-full ">
+      <div className=" bg-gray-700  ">
+      <table className="hover-table-fixed max-w-[500px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg text-white text-center">
+        <thead className = "text-2x1 dark:text-white font-bold text-center">
+          <tr className=" bg-gray-700  ">
+            <th class="border border-slate-250 " >Nombre</th>
+            <th class="border border-slate-250" >Apellido</th>
+            <th class="border border-slate-250">Telefono</th>
+            <th class="border border-slate-250">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="border border-slate-600 ">Miguel Ángel </td>
+            <td class="border border-slate-600 ">Parra </td>
+            <td class="border border-slate-600 ">2932569</td>
+            <td class="border border-slate-600 text-xs ">MiguelAngel@gmail.com</td>
+          </tr>
+          <tr>
+            <td class="border border-slate-600 ">Juan Carlos </td>
+            <td class="border border-slate-600 ">Lopez</td>
+            <td  class="border border-slate-600 ">2984123</td>
+            <td  class="border border-slate-600 ">@gmail.com</td>
+          </tr>
+          <tr>
+            <td class="border border-slate-600 ">Luciano Martin</td>
+            <td class="border border-slate-600 ">Velazques</td>
+            <td class="border border-slate-600 ">3432345</td>
+            <td  class="border border-slate-600 ">@gmail.com</td>
+          </tr>
+          <tr>
+            <td class="border border-slate-600 ">Patricio</td>
+            <td class="border border-slate-600 ">Ordoñe</td>
+            <td class="border border-slate-600 ">3431234</td>
+            <td class="border border-slate-600 ">ElPati@gmail.com</td>
+          </tr>
+          
+        </tbody>
+      </table>
+      </div>
+      <div className="bg-gray-700 flex flex-col justify-center">
         {/* <div className='hidden sm:block'>
         <img className='w-full h-full object-cover' src={loginImg} alt=""></img>
       </div> */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
               </svg>
               <span class="flex-1 ml-3 whitespace-nowrap">Persona</span>
               <span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                Pro
+                m
               </span>
             </a>
           </li>
