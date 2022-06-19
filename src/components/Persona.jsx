8c848891c3f5149373e4308685dab7158f1ffa80
Laucha0550 <ivangabrielf971@gmail.com>
2022-06-19 01:00:54 -0300

    Arrerglo de todos los errores de ellos

--- src/components/Persona.jsx
+++ src/components/Persona.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const Cliente = () => {
+const Persona = () => {
   return (
     <div>
       <div className="sm:grid-cols-2 w-screen h-full">
@@ -29,30 +29,6 @@ const Cliente = () => {
                 name="nomUsuario"
               />
             </div>
-            <div className="flex flex-col text-gray-400 py-2">
-              <label>Telefono</label>
-              <input
-                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
-                type="text"
-                name="contraseña"
-              />
-            </div>
-            <div className="flex flex-col text-gray-400 py-2">
-              <label>Email</label>
-              <input
-                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
-                type="text"
-                name="nomUsuario"
-              />
-            </div>
-            <div className="flex flex-col text-gray-400 py-2">
-              <label>Genero</label>
-              <input
-                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
-                type="text"
-                name="contraseña"
-              />
-            </div>
             <button className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg">
               Registrar
             </button>
@@ -62,4 +38,4 @@ const Cliente = () => {
     </div>
   );
 };
-export default Cliente;
+export default Persona;
