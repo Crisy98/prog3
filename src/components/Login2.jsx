9ca29a29e7c49d92bab043946a9499cfd7d23a3a
Ana Molinelli <anamolinelli@gmail.com>
2022-06-28 19:25:54 -0300

    Boton degrade login

--- src/components/Login2.jsx
+++ src/components/Login2.jsx
@@ -112,7 +112,9 @@ function Login2() {
               <p>Contraseña olvidada</p>
             </div>
             <div className="  ">
-              <input className=" w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg " type="submit" />
+              <input className="text-white bg-gradient-to-r from-cyan-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-950 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2 w-full my-5 py-2" type="submit" />
+              {/* <input className=" w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg " type="submit" /> */}
+              {/* "text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" */}
             </div>
             {/* <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Ingresar</button> */}
           </form>
