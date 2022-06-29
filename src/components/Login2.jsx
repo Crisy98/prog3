522a1f4a35d25772ac3817a6519e1ac081d21f6b
Laucha0550 <ivangabrielf971@gmail.com>
2022-06-29 14:42:34 -0300

    Navbar

--- src/components/Login2.jsx
+++ src/components/Login2.jsx
@@ -113,31 +113,13 @@ function Login2() {
             </div>
             <div className="  ">
               <input className="text-white bg-gradient-to-r from-cyan-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-950 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2 w-full my-5 py-2" type="submit" />
-              {/* <input className=" w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg " type="submit" /> */}
-              {/* "text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" */}
+              
             </div>
-            {/* <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/20 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Ingresar</button> */}
+            
           </form>
         </div>
       </div>
     </div>
-    // <div className="form">
-    //   <form onSubmit={handleSubmit}>
-    //     <div className="input-container">
-    //       <label>Username </label>
-    //       <input type="text" name="uname" required />
-    //       {renderErrorMessage("uname")}
-    //     </div>
-    //     <div className="input-container">
-    //       <label>Password </label>
-    //       <input type="password" name="pass" required />
-    //       {renderErrorMessage("pass")}
-    //     </div>
-    //     <div className="button-container">
-    //       <input type="submit"  />
-    //     </div>
-    //   </form>
-    // </div>
   );
 
   return (
